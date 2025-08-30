baf357685985b6953b3c9e494839f4aa9d0cdb6d
이석현 <jrkenniya@gmail.com>
2025-08-30 19:34:41 +0900

    [refactor] 소비기한 메뉴 - Resource 클래스 2개로 분리, 메시지 일부 수정

--- resources/date.py
+++ resources/date.py
@@ -4,163 +4,227 @@ import mysql.connector
 from mysql_connection import get_connection
 from datetime import datetime
 
-class DateResource(Resource):
-    def get(self, purchase_id=None):
-        # 소비기한 - 소비기한 상세 정보 조회 ✅
-        if purchase_id:
-            device_id = request.args.get('device_id')
-
-            if device_id == None or purchase_id == None:
-                response = {
-                    "error_code" : 400,
-                    "description" : "Bad Request",
-                    "message" : f"필수 파라미터 누락 : {str(e)}"
-                }
-                return jsonify(response), 400
-
-            try:
-                connection = get_connection()
-                query = '''
-                    select 
-                        ProductItem.item_id,
-                        Product.product_name,
-                        ProductItem.expiration_date,
-                        Purchase.storage_location,
-                        ProductItem.ingredients,
-                        ProductItem.summary
-                    from Purchase
-                    join ProductItem on Purchase.purchase_id = ProductItem.item_id
-                    join Product on ProductItem.product_id = Product.product_id
-                    where Purchase.purchase_id = %s and Purchase.device_id = %s;
-                '''
-                cursor = connection.cursor(dictionary=True)
-                cursor.execute(query, (purchase_id, device_id))
-
-                if cursor.rowcount == 0:
-                    response = {
-                        "error_code": 404,
-                        "description": "Not Found",
-                        "message": f"해당 제품을 찾을 수 없음 : {str(e)}"
-                    }
-                    return jsonify(response), 404
-
-                result = cursor.fetchone()
-                cursor.close()
-                connection.close()
-
-            except mysql.connector.Error as e :
-                print(e)
-                cursor.close()
-                connection.close()
-                response = {
-                    "error_code" : 503,
-                    "description" : e.description,
-                    "message" : f"MySQL connector 에러 : {str(e)}"
-                }
-                return jsonify(response), 503 # HTTPStatus.SERVICE_UNAVAILABLE
-            
-            except Exception as e :
-                print(e)
-                cursor.close()
-                connection.close()
-                response = {
-                    "error_code" : 500,
-                    "description" : e.description,
-                    "message" : f"서버 내부 오류 : {str(e)}"
-                }
-                return jsonify(response), 500
-    
-            return{
-                "success" : True,
-                "status" : 200,
-                "message" : "상세 조회 성공",
-                "data" : result
-            }, 200
-
+class DateListResource(Resource):
+    def get(self):
         # 소비기한 - 소비기한 리스트 조회 ✅
-        else:
-            device_id = request.args.get('device_id')
-            category = request.args.get('category')
+        device_id = request.args.get('device_id')
+        category = request.args.get('category')
 
-            if device_id == None or category == None:
+        if device_id == None or category == None:
+            response = {
+                "error_code" : 400,
+                "description" : "Bad Request",
+                "message" : "필수 파라미터 누락"
+            }
+            return jsonify(response), 400
+        
+        try:
+            connection = get_connection()
+            query = '''
+                select 
+                    ProductItem.item_id,
+                    Product.product_name,
+                    ProductItem.expiration_date,
+                    Purchase.is_favorite
+                from Purchase
+                join ProductItem on Purchase.purchase_id = ProductItem.item_id
+                join Product on ProductItem.product_id = Product.product_id
+                where Purchase.device_id = %s;
+            '''
+            record = (device_id, )
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute(query, record)
+
+            if cursor.rowcount == 0:
                 response = {
-                    "error_code" : 400,
-                    "description" : "Bad Request",
-                    "message" : f"필수 파라미터 누락 : {str(e)}"
+                    "error_code": 404,
+                    "description": "Not Found",
+                    "message": "해당 조건에 맞는 제품이 없음"
                 }
-                return jsonify(response), 400
-            
-            try:
-                connection = get_connection()
-                query = '''
-                    select 
-                        ProductItem.item_id,
-                        Product.product_name,
-                        ProductItem.expiration_date,
-                        Purchase.is_favorite
-                    from Purchase
-                    join ProductItem on Purchase.purchase_id = ProductItem.item_id
-                    join Product on ProductItem.product_id = Product.product_id
-                    where Purchase.device_id = %s;
-                '''
-                record = (device_id, )
-                cursor = connection.cursor(dictionary=True)
-                cursor.execute(query, record)
+                return jsonify(response), 404
 
-                if cursor.rowcount == 0:
-                    response = {
-                        "error_code": 404,
-                        "description": "Not Found",
-                        "message": f"해당 조건에 맞는 제품이 없음 : {str(e)}"
-                    }
-                    return jsonify(response), 404
+            result_list = cursor.fetchall()
+            filtered_list = []
 
-                result_list = cursor.fetchall()
-                filtered_list = []
+            for result in result_list:
+                now_date = datetime.now()
+                db_date = datetime.strptime(result['expiration_date'], "%Y%m%d")
+                day_diff = (now_date - db_date).days
+                if category == '날짜 지남' and day_diff < 0:
+                    filtered_list.append(result)
+                elif category == '기한 임박' and day_diff < 7:
+                    filtered_list.append(result)
+                elif category == '기한 여유' and day_diff < 14:
+                    filtered_list.append(result)
 
-                for result in result_list:
-                    now_date = datetime.now()
-                    db_date = datetime.strptime(result['expiration_date'], "%Y%m%d")
-                    day_diff = (now_date - db_date).days
-                    if category == '날짜 지남' and day_diff < 0:
-                        filtered_list.append(result)
-                    elif category == '기한 임박' and day_diff < 7:
-                        filtered_list.append(result)
-                    elif category == '기한 여유' and day_diff < 14:
-                        filtered_list.append(result)
+            cursor.close()
+            connection.close()
 
-                cursor.close()
-                connection.close()
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            response = {
+                "error_code" : 503,
+                "description" : e.description,
+                "message" : f"MySQL connector 에러 : {str(e)}"
+            }
+            return jsonify(response), 503 # HTTPStatus.SERVICE_UNAVAILABLE
+        
+        except Exception as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            response = {
+                "error_code" : 500,
+                "description" : e.description,
+                "message" : f"서버 내부 오류 : {str(e)}"
+            }
+            return jsonify(response), 500
 
-            except mysql.connector.Error as e :
-                print(e)
-                cursor.close()
-                connection.close()
-                response = {
-                    "error_code" : 503,
-                    "description" : e.description,
-                    "message" : f"MySQL connector 에러 : {str(e)}"
-                }
-                return jsonify(response), 503 # HTTPStatus.SERVICE_UNAVAILABLE
-            
-            except Exception as e :
-                print(e)
-                cursor.close()
-                connection.close()
-                response = {
-                    "error_code" : 500,
-                    "description" : e.description,
-                    "message" : f"서버 내부 오류 : {str(e)}"
-                }
-                return jsonify(response), 500
+        return{
+            "success" : True,
+            "status" : 200,
+            "message" : "리스트 조회 성공",
+            "data" : filtered_list
+        }, 200
     
-            return{
-                "success" : True,
-                "status" : 200,
-                "message" : "리스트 조회 성공",
-                "data" : filtered_list
-            }, 200
+    # 소비기한 - 소비기한 특정 제품 삭제 ✅
+    def delete(self):
+        data = request.get_json()
+        if 'device_id' or 'purchase_id' not in data:
+            response = {
+                "error_code" : 400,
+                "description" : "Bad Request",
+                "message" : "필수 파라미터 누락"
+            }
+            return jsonify(response), 400
+
+        device_id = data.get('device_id')
+        purchase_ids = data.get('purchase_id')
+
+        try :
+            connection = get_connection()
+            placeholders = ','.join(['%s'] * len(purchase_ids))
+            query = f'''
+                    delete from purchase
+                    where device_id = %s AND purchase_id IN ({placeholders});
+                    '''
+            record = [device_id] + purchase_ids
+            cursor = connection.cursor()
+            cursor.execute(query, tuple(record))
+            connection.commit()
+
+            if cursor.rowcount == 0:
+                response = {
+                    "error_code": 404,
+                    "description": "Not Found",
+                    "message": "삭제할 제품을 찾을 수 없음"
+                }
+                return jsonify(response), 404
+            
+            cursor.close()
+            connection.close()
+ 
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            response = {
+                "error_code" : 503,
+                "description" : e.description,
+                "message" : f"MySQL connector 에러 : {str(e)}"
+            }
+            return jsonify(response), 503 # HTTPStatus.SERVICE_UNAVAILABLE
+        
+        except Exception as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            response = {
+                "error_code" : 500,
+                "description" : e.description,
+                "message" : f"서버 내부 오류 : {str(e)}"
+            }
+            return jsonify(response), 500
+ 
+        return{
+            "success" : True,
+            "status" : 200,
+            "message" : "제품 삭제 성공"
+        }, 200
     
+class DateItemResource(Resource):
+    def get(self, purchase_id):
+        # 소비기한 - 소비기한 상세 정보 조회 ✅
+        device_id = request.args.get('device_id')
+
+        if device_id == None or purchase_id == None:
+            response = {
+                "error_code" : 400,
+                "description" : "Bad Request",
+                "message" : "필수 파라미터 누락"
+            }
+            return jsonify(response), 400
+
+        try:
+            connection = get_connection()
+            query = '''
+                select 
+                    ProductItem.item_id,
+                    Product.product_name,
+                    ProductItem.expiration_date,
+                    Purchase.storage_location,
+                    ProductItem.ingredients,
+                    ProductItem.summary
+                from Purchase
+                join ProductItem on Purchase.purchase_id = ProductItem.item_id
+                join Product on ProductItem.product_id = Product.product_id
+                where Purchase.purchase_id = %s and Purchase.device_id = %s;
+            '''
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute(query, (purchase_id, device_id))
+
+            if cursor.rowcount == 0:
+                response = {
+                    "error_code": 404,
+                    "description": "Not Found",
+                    "message": "해당 제품을 찾을 수 없음"
+                }
+                return jsonify(response), 404
+
+            result = cursor.fetchone()
+            cursor.close()
+            connection.close()
+
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            response = {
+                "error_code" : 503,
+                "description" : e.description,
+                "message" : f"MySQL connector 에러 : {str(e)}"
+            }
+            return jsonify(response), 503 # HTTPStatus.SERVICE_UNAVAILABLE
+        
+        except Exception as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            response = {
+                "error_code" : 500,
+                "description" : e.description,
+                "message" : f"서버 내부 오류 : {str(e)}"
+            }
+            return jsonify(response), 500
+
+        return{
+            "success" : True,
+            "status" : 200,
+            "message" : "상세 조회 성공",
+            "data" : result
+        }, 200
     
     def patch(self, purchase_id):
         data = request.get_json()
@@ -171,7 +235,7 @@ class DateResource(Resource):
                 response = {
                     "error_code" : 400,
                     "description" : "Bad Request",
-                    "message" : f"필수 파라미터 누락 : {str(e)}"
+                    "message" : "필수 파라미터 누락"
                 }
                 return jsonify(response), 400
 
@@ -190,7 +254,7 @@ class DateResource(Resource):
                     response = {
                         "error_code": 404,
                         "description": "Not Found",
-                        "message": f"수정할 제품을 찾을 수 없음 : {str(e)}"
+                        "message": "수정할 제품을 찾을 수 없음"
                     }
                     return jsonify(response), 404
                 
@@ -234,7 +298,7 @@ class DateResource(Resource):
                 response = {
                     "error_code" : 400,
                     "description" : "Bad Request",
-                    "message" : f"필수 파라미터 누락 : {str(e)}"
+                    "message" : "필수 파라미터 누락"
                 }
                 return jsonify(response), 400
 
@@ -254,7 +318,7 @@ class DateResource(Resource):
                     response = {
                         "error_code": 404,
                         "description": "Not Found",
-                        "message": f"수정할 제품을 찾을 수 없음 : {str(e)}"
+                        "message": "수정할 제품을 찾을 수 없음"
                     }
                     return jsonify(response), 404
 
@@ -289,69 +353,3 @@ class DateResource(Resource):
                 "status" : 200,
                 "message" : "즐겨찾기 업데이트 성공"
             }, 200
-    
-    # 소비기한 - 소비기한 특정 제품 삭제 ✅
-    def delete(self):
-        data = request.get_json()
-
-        if 'device_id' or 'purchase_id' not in data:
-            response = {
-                "error_code" : 400,
-                "description" : "Bad Request",
-                "message" : f"필수 파라미터 누락 : {str(e)}"
-            }
-            return jsonify(response), 400
-
-        device_id = data.get('device_id')
-        purchase_ids = data.get('purchase_id')
-
-        try :
-            connection = get_connection()
-            placeholders = ','.join(['%s'] * len(purchase_ids))
-            query = f'''
-                    delete from purchase
-                    where device_id = %s AND purchase_id IN ({placeholders});
-                    '''
-            record = [device_id] + purchase_ids
-            cursor = connection.cursor()
-            cursor.execute(query, tuple(record))
-            connection.commit()
-
-            if cursor.rowcount == 0:
-                response = {
-                    "error_code": 404,
-                    "description": "Not Found",
-                    "message": f"삭제할 제품을 찾을 수 없음 : {str(e)}"
-                }
-                return jsonify(response), 404
-            
-            cursor.close()
-            connection.close()
- 
-        except mysql.connector.Error as e :
-            print(e)
-            cursor.close()
-            connection.close()
-            response = {
-                "error_code" : 503,
-                "description" : e.description,
-                "message" : f"MySQL connector 에러 : {str(e)}"
-            }
-            return jsonify(response), 503 # HTTPStatus.SERVICE_UNAVAILABLE
-        
-        except Exception as e :
-            print(e)
-            cursor.close()
-            connection.close()
-            response = {
-                "error_code" : 500,
-                "description" : e.description,
-                "message" : f"서버 내부 오류 : {str(e)}"
-            }
-            return jsonify(response), 500
- 
-        return{
-            "success" : True,
-            "status" : 200,
-            "message" : "제품 삭제 성공"
-        }, 200
--- run.py
+++ run.py
@@ -3,7 +3,8 @@ from flask import Flask, request, jsonify
 from flask_restful import Api
 from resources.user import UserResource
 from resources.product import ProductsResource
-from resources.date import DateResource
+from resources.date import DateListResource
+from resources.date import DateItemResource
 from resources.analyze import AnalyzeResource
 from error_handler import error_app
 
@@ -23,7 +24,8 @@ api = Api(app)
 api.add_resource(UserResource, '/user') # 로그인, 회원 탈퇴
 api.add_resource(AnalyzeResource, "/analyze") # 촬영하기 - 이미지 분석 진행
 api.add_resource(ProductsResource, '/products/<int:purchase_id>') # 촬영하기 - 제품 보관하기
-api.add_resource(DateResource, "/date", "/date/<int:purchase_id>") # 소비기한 메뉴
+api.add_resource(DateListResource, "/date") # 소비기한 메뉴 - 리스트 조회, 특정 제품 삭제
+api.add_resource(DateItemResource, "/date/<int:purchase_id>") # 소비기한 메뉴 - 상세 정보 조회, 보관 장소 수정, 즐겨찾기 업데이트
 
 # ----------------------------
 # 서버 실행
