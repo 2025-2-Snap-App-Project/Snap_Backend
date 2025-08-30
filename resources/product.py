d62f6c97dbac18778f63dd47b94b7f9590c47ebc
이석현 <jrkenniya@gmail.com>
2025-08-30 19:50:33 +0900

    [refator] 소비기한 보관 장소 수정 - URL 수정

--- resources/product.py
+++ resources/product.py
@@ -3,8 +3,8 @@ from flask_restful import Resource
 import mysql.connector
 from mysql_connection import get_connection
 
-# 촬영하기 - 제품 보관하기 ✅
 class ProductsResource(Resource):
+    # 촬영하기 - 제품 보관하기 ✅
     def post(self, purchase_id):
         data = request.get_json()
         if 'device_id' not in data or 'storage_location' not in data or purchase_id == None :
@@ -57,4 +57,67 @@ class ProductsResource(Resource):
             "success" : True,
             "status" : 200,
             "message" : "보관 장소 등록 성공"
+        }, 200
+    
+    # 소비기한 - 소비기한 보관 장소 수정 ✅
+    def patch(self, purchase_id):
+        data = request.get_json()
+        if purchase_id == None or 'device_id' not in data or 'storage_location' not in data:
+            response = {
+                "error_code" : 400,
+                "description" : "Bad Request",
+                "message" : "필수 파라미터 누락"
+            }
+            return jsonify(response), 400
+
+        try :
+            connection = get_connection()
+            query = '''
+                UPDATE purchase
+                SET storage_location = %s
+                WHERE device_id = %s AND purchase_id = %s
+            '''
+            record = (data['storage_location'], data['device_id'], purchase_id)
+            cursor = connection.cursor()
+            cursor.execute(query, record)
+
+            if cursor.rowcount == 0:
+                response = {
+                    "error_code": 404,
+                    "description": "Not Found",
+                    "message": "수정할 제품을 찾을 수 없음"
+                }
+                return jsonify(response), 404
+            
+            connection.commit()
+            cursor.close()
+            connection.close()
+        
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            response = {
+                "error_code" : 503,
+                "description" : e.description,
+                "message" : f"MySQL connector 에러 : {str(e)}"
+            }
+            return jsonify(response), 503 # HTTPStatus.SERVICE_UNAVAILABLE
+        
+        except Exception as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            response = {
+                "error_code" : 500,
+                "description" : e.description,
+                "message" : f"서버 내부 오류 : {str(e)}"
+            }
+            return jsonify(response), 500
+
+
+        return{
+            "success" : True,
+            "status" : 200,
+            "message" : "보관 장소 수정 성공"
         }, 200
--- run.py
+++ run.py
@@ -23,9 +23,9 @@ app = Flask(__name__)
 api = Api(app)
 api.add_resource(UserResource, '/user') # 로그인, 회원 탈퇴
 api.add_resource(AnalyzeResource, "/analyze") # 촬영하기 - 이미지 분석 진행
-api.add_resource(ProductsResource, '/products/<int:purchase_id>') # 촬영하기 - 제품 보관하기
+api.add_resource(ProductsResource, '/products/<int:purchase_id>') # 촬영하기 - 제품 보관하기, 소비기한 메뉴 - 보관 장소 수정
 api.add_resource(DateListResource, "/date") # 소비기한 메뉴 - 리스트 조회, 특정 제품 삭제
-api.add_resource(DateItemResource, "/date/<int:purchase_id>") # 소비기한 메뉴 - 상세 정보 조회, 보관 장소 수정, 즐겨찾기 업데이트
+api.add_resource(DateItemResource, "/date/<int:purchase_id>") # 소비기한 메뉴 - 상세 정보 조회, 즐겨찾기 업데이트
 
 # ----------------------------
 # 서버 실행
