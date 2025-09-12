39709df5b63924e59ba748ca9c5acaa2a5f4a939
이석현 <jrkenniya@gmail.com>
2025-09-12 13:16:47 +0900

    [refactor] 로그인, 회원탈퇴 - try, except, finally문으로 변경
    db cursor와 db connection을 close하는 역할을 finally문에서 담당

--- resources/user.py
+++ resources/user.py
@@ -25,29 +25,25 @@ class UserResource(Resource) :
             cursor = connection.cursor()
             cursor.execute(query, record)
             connection.commit()
-            cursor.close()
-            connection.close()
+
+            return {
+                "success" : True,
+                "status" : 200,
+                "message" : "로그인 성공"
+            }, 200
 
         except mysql.connector.errors.IntegrityError as e:
-            cursor.close()
-            connection.close()
             handle_mysql_integrity_error(e, "동일 디바이스 ID가 이미 존재합니다. 다시 시도해주세요.")
         
         except mysql.connector.Error as e :
-            cursor.close()
-            connection.close()
             handle_mysql_integrity_error(e)
         
         except Exception as e :
+            server_error(e)
+
+        finally:
             cursor.close()
             connection.close()
-            server_error(e)
-        
-        return{
-            "success" : True,
-            "status" : 200,
-            "message" : "로그인 성공"
-        }, 200
     
     # 회원 탈퇴 ✅
     def delete(self) :
@@ -70,27 +66,22 @@ class UserResource(Resource) :
 
             if cursor.rowcount == 0:
                 handle_not_found_error("해당 디바이스 ID의 사용자를 찾을 수 없음")
-            
-            cursor.close()
-            connection.close()
+
+            return {
+                "success" : True,
+                "status" : 200,
+                "message" : "회원탈퇴 성공"
+            }, 200
 
         except mysql.connector.errors.IntegrityError as e:
-            cursor.close()
-            connection.close()
             handle_mysql_integrity_error(e, "회원 탈퇴에 실패했습니다. 다시 시도해주세요.")
 
         except mysql.connector.Error as e :
-            cursor.close()
-            connection.close()
             handle_mysql_connect_error(e)
         
         except Exception as e :
-            cursor.close()
-            connection.close()
             server_error(e)
 
-        return{
-            "success" : True,
-            "status" : 200,
-            "message" : "회원탈퇴 성공"
-        }, 200
+        finally:
+            cursor.close()
+            connection.close()
