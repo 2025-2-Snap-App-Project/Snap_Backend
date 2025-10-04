58472d9b64b657d8b8afa794e4bbb4f85dabfa60
이석현 <jrkenniya@gmail.com>
2025-10-04 13:26:40 +0900

    [refactor] 400 Bad Request 에러 핸들링 함수 구조 변경

--- error_handler.py
+++ error_handler.py
@@ -1,12 +1,10 @@
 # 400 Bad Request - 필수 파라미터 누락
-def handle_value_error(data, required_fields):
-    for field in required_fields:
-        if field not in data:
-            return {
-                "error_code" : 400,
-                "description" : "Bad Request",
-                "message" : f"{field} 누락"
-            }, 400
+def handle_value_error(msg):
+    return {
+        "error_code" : 400,
+        "description" : "Bad Request",
+        "message" : msg
+    }, 400
 
 # 415 Unsupported Media Type
 def handle_media_type_error(msg):
