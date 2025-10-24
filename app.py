22612d6c8458fb20d41a1f2f501012c7f62ad3b6
이석현 <jrkenniya@gmail.com>
2025-10-25 00:57:39 +0900

    [refactor] run.py -> app.py로 변경, 외부 접근 가능하도록 수정

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ api.add_resource(AnalyzeResource, "/analyze") # 촬영하기 - 이미지 분석 
 # ----------------------------
 # 서버 실행
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
