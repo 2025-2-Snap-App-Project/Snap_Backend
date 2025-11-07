bdeadb9d2cf34f5d0f4df9e066bc58dd9b8dbb39
이석현 <jrkenniya@gmail.com>
2025-11-07 20:09:08 +0900

    [chore] Google Cloud Vision API 서비스키 json 경로 연결

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     google_api_key: str
+    google_application_credentials: str
     debug: bool = False
 
     class Config:
