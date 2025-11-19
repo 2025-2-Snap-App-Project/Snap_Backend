340bb9294973d650b2a437251485be2313c96f00
이석현 <jrkenniya@gmail.com>
2025-11-19 19:23:00 +0900

    [feat] app.py에도 변경된 클래스명+파일명을 적용

--- app.py
+++ app.py
@@ -6,7 +6,7 @@
 from flask import Flask
 from flask_restful import Api
 from resources.analyze import AnalyzeResource
-from resources.real_time import RealTimeResource
+from resources.name import ProductNameResource
 
 # ----------------------------
 # Flask 앱 생성
@@ -15,7 +15,7 @@ app = Flask(__name__)
 # restfulAPI 생성
 api = Api(app)
 api.add_resource(AnalyzeResource, "/analyze") # 촬영하기 - 이미지 분석 진행
-api.add_resource(RealTimeResource, "/realtime") # 촬영하기 - 실시간 제품명 안내
+api.add_resource(ProductNameResource, "/name") # 촬영하기 - 제품명 안내
 
 # ----------------------------
 # 서버 실행
